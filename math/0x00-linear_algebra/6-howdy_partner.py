b0c6d7bd10df40987edbc1cafe58679cd60239a2
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-03-10 13:04:10 +0100

    Create 6-howdy_partner.py

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,18 @@
+alexa@ubuntu-xenial:0x00-linear_algebra$ cat 6-main.py 
+#!/usr/bin/env python3
+
+def cat_arrays(arr1, arr2):
+    return arr1 + arr2
+
+cat_arrays = __import__('6-howdy_partner').cat_arrays
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [6, 7, 8]
+print(cat_arrays(arr1, arr2))
+print(arr1)
+print(arr2)
+alexa@ubuntu-xenial:0x00-linear_algebra$ ./6-main.py 
+[1, 2, 3, 4, 5, 6, 7, 8]
+[1, 2, 3, 4, 5]
+[6, 7, 8]
+alexa@ubuntu-xenial:0x00-linear_algebra$ 
