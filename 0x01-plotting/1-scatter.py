4b0b3550e29fd9bec065fd1edf7bca8c2af2dfbf
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-03-12 08:52:27 +0100

    Update 1-scatter.py

--- 0x01-plotting/1-scatter.py
+++ 0x01-plotting/1-scatter.py
@@ -7,3 +7,9 @@ cov = [[15, 8], [8, 15]]
 np.random.seed(5)
 x, y = np.random.multivariate_normal(mean, cov, 2000).T
 y += 180
+
+plt.scatter(x, y, c='m')
+plt.title("Men's Height vs Weight")
+plt.xlabel('Height (in)')
+plt.ylabel('Weight (lbs)')
+plt.show()
