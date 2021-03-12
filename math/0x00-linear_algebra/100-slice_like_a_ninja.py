f589a156f5317da88161d04b1777f7f290b0e287
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-03-12 16:30:20 +0100

    Update 100-slice_like_a_ninja.py

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-  
+
 import numpy as np
 
 def np_slice(matrix, axes={}):
     result = []
-    i = 0
-    for key, value in axes.items():
-        while i < key:
-            result.append(slice(None))
-            i += 1
-        result.append(slice(*value))
-        i += 1
+    maxx = max(axes)
+    for i in range(maxx + 1):
+        if i in axes.keys():
+            sliced.append(slice(*axes.get(i)))
+        else:
+            result.append(slice(None, None, None))
     return matrix[tuple(result)]
 
 
