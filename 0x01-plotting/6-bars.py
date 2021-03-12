4448ab2e93701e54711588edf44e498e3fe1aa50
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-03-12 22:55:17 +0100

    Update 6-bars.py

--- 0x01-plotting/6-bars.py
+++ 0x01-plotting/6-bars.py
@@ -6,12 +6,12 @@ np.random.seed(5)
 fruit = np.random.randint(0, 20, (4,3))
 
 names = ['Farrah', 'Fred', 'Felicia']
-fs = ['apples', 'bananas', 'oranges', 'peaches']
+fruits = ['apples', 'bananas', 'oranges', 'peaches']
 colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
 
 for i in range(len(fruit)):
     plt.bar(names, fruit[i], bottom=np.sum(fruit[:i], axis=0),
-            color=colors[i], label=fs[i], width=0.5)
+            color=colors[i], label=fruits[i], width=0.5)
 
 plt.title("Number of Fruit per Person")
 plt.ylabel("Quantity of Fruit")
