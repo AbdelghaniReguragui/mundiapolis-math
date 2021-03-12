6747719b9b29a275599c8a51ad9286a3b8b359b3
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-03-12 15:06:47 +0100

    Update 3-flip_me_over.py

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -4,7 +4,7 @@ def matrix_transpose(matrix):
     tmp = []
     for x in range(len(matrix[0])):
         b = []
-    for y in range(len(matrix)):
-        b.append(matrix[y][x])
+        for y in range(len(matrix)):
+            b.append(matrix[y][x])
         tmp.append(b)
     return tmp
