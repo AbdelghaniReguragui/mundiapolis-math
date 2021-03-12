49a4b7c2240f8a9169e3f6fca8dc0c589cf43c60
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-03-12 15:07:59 +0100

    Update 4-line_up.py

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,4 +1,3 @@
-alexa@ubuntu-xenial:0x00-linear_algebra$ cat 4-main.py 
 #!/usr/bin/env python3
 
 def add_arrays(arr1, arr2):
@@ -9,18 +8,3 @@ def add_arrays(arr1, arr2):
         return arr
     else :
         return None
-
-add_arrays = __import__('4-line_up').add_arrays
-
-arr1 = [1, 2, 3, 4]
-arr2 = [5, 6, 7, 8]
-print(add_arrays(arr1, arr2))
-print(arr1)
-print(arr2)
-print(add_arrays(arr1, [1, 2, 3]))
-alexa@ubuntu-xenial:0x00-linear_algebra$ ./4-main.py 
-[6, 8, 10, 12]
-[1, 2, 3, 4]
-[5, 6, 7, 8]
-None
-alexa@ubuntu-xenial:0x00-linear_algebra$ 
