b7e14bfedb93156d0a72cbdbf57b9119d28c8803
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-03-12 17:08:38 +0100

    Update 102-squashed_like_sardines.py

--- math/0x00-linear_algebra/102-squashed_like_sardines.py
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 
-import numpy as np
-import time
-
 def matrix_shape(matrix):
     if type(matrix[0]) is not list:
-        return [len(matrix)]
+        return len(matrix)
     else:
-        return [len(matrix)] + matrix_shape(matrix[0])
+        return len(matrix) + matrix_shape(matrix[0])
 
 
 def concat_recursive(mat1, mat2, axe):
