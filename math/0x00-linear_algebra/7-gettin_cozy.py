483f2b83b4d300f9eca5076f3fdde3c477f32a91
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-03-12 15:26:23 +0100

    Update 7-gettin_cozy.py

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -6,7 +6,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
         tmp2 = [x[:] for x in mat2]
         result = tmp1 + tmp2
         return result
-    elif (len(mat1) == len(mat2)) and axis == 1:
+    elif(len(mat1) == len(mat2)) and axis == 1:
         result = [mat1[x] + mat2[x] for x in range(len(mat1))]
         return result
     else:
