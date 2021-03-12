15e4ab13437f559b5b28f5c3c9b248782ab052f7
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-03-12 15:16:57 +0100

    Update 5-across_the_planes.py

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def add_matrices2D(mat1, mat2):
-    if (len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0])):
+    if(len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0])):
         result = []
         for i in range (len(mat1)):
             tmp = []
