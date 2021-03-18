d0557423fb69137c5e010423d67a1983cd338d1f
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-03-18 22:08:09 +0100

    Update 2-size_me_please.py

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 """
-aaaaaaaaaaaaaa 
+dsfa fdsad abjn a 
 """
 def matrix_shape(matrix):
-    """ ssssssss """
     if type(matrix[0]) is not list:
         return [len(matrix)]
-    """
-    fffffff
-    """
     else:
         return [len(matrix)] + matrix_shape(matrix[0])
