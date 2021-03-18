8c8abf0b2d80a31f5f1df6628c9f3464ee47d445
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-03-18 22:01:39 +0100

    Update 2-size_me_please.py

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+''' la fonction matrix_shape prend comme parametres une matrice'''
 def matrix_shape(matrix):
+    ''' si la matrice ne contient qu'une seule ligne ca retourne la longueur de la ligne '''
     if type(matrix[0]) is not list:
         return [len(matrix)]
+    ''' sinon  ca calcul le nombre de ligne + colonne '''
     else:
         return [len(matrix)] + matrix_shape(matrix[0])
