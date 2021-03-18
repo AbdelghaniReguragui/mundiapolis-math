0e230069be48853e67acd2f9ad983f828c431e0d
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-03-18 21:50:54 +0100

    Update 1-trim_me_down.py

--- math/0x00-linear_algebra/1-trim_me_down.py
+++ math/0x00-linear_algebra/1-trim_me_down.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-#La matrice '''
+#La matrice
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = []
 #La boucle for
 for x in range(3):
-    # il prend x[2], x[3] de chaque ligne '''
+    #il prend x[2], x[3] de chaque ligne
     the_middle.append(matrix[x][2:4])
 print("The middle columns of the matrix are: {}".format(the_middle))
