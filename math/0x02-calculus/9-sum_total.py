8e34919529e22b6fc549d6ad512aeb0ac76a8a59
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-04-16 16:56:27 +0200

    Create 9-sum_total.py

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def summation_i_squared(n):
+    #If n is not a valid number, return None
+    if type(n) != int or n < 1:
+        return None
+    #Else do
+    else:
+        #if n > 1, do
+        if n > 1:
+            return n**2 + summation_i_squared(n-1)
+        #when n == 1, return result
+        return n
