d8d90e54a1100b7cc41be6b412c1ff4b8289f708
AbdelghaniReguragui <77926814+AbdelghaniReguragui@users.noreply.github.com>
2021-04-16 16:57:38 +0200

    Update 9-sum_total.py

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -3,7 +3,6 @@ def summation_i_squared(n):
     #If n is not a valid number, return None
     if type(n) != int or n < 1:
         return None
-    #Else do
     else:
         #if n > 1, do
         if n > 1:
